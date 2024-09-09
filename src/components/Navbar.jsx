835b79afd0009f80d519810b1668ba9430b2d541
André Rosatto <linus1178@gmail.com>
2024-09-09 12:45:52 -0300

    navbar links improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
 export default function Navbar({ index }) {
-	const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-	const links = [
-		{ title: 'Home', url: '/' },
-		{ title: 'About Me', url: 'about' },
-		{ title: 'Buzzvel Projects', url: 'projects' },
-		{ title: 'Career', url: 'career' },
-	];
+  const links = [
+    { title: 'Home', url: '/' },
+    { title: 'About Me', url: '/about' },
+    { title: 'Buzzvel Projects', url: '/projects' },
+    { title: 'Career', url: '/career' },
+  ];
 
-	return (
-		<nav className={`Navbar ${expanded ? 'expand' : ''}`}>
-			<div className="menu" onClick={() => setExpanded(!expanded)}></div>
-			<ul>
-				{links.map((link, idx) => <li key={idx} className={idx === index ? 'selected' : ''}><a href={link.url}>{link.title}</a></li>)}
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className={`Navbar ${expanded ? 'expand' : ''}`}>
+      <div className='menu' onClick={() => setExpanded(!expanded)}></div>
+      <ul>
+        {links.map((link, idx) => (
+          <li key={idx} className={idx === index ? 'selected' : ''}>
+            <Link to={link.url}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
